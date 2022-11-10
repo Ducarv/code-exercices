6e84fe31367ac8bbca0efab9882ea1ac2a5b6978
ducarv <educarvalho009@gmail.com>
2022-11-10 06:23:44 -0400

    I need to fix the 'removePerson' method

--- day007-POO/ex011.js
+++ day007-POO/ex011.js
@@ -1,8 +1,5 @@
 class Planner {
-    constructor(name, age, height) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
+    constructor() {
         this.plannerArr = [];
     }
 
@@ -16,10 +13,59 @@ class Planner {
     }
 
     removePerson(name) {
-        for(let count = 0; count < this.plannerArr.length; count++) {
+        let count = 0;
+        while(count < this.plannerArr.length) {
             if(name === this.plannerArr[count].name) {
                 this.plannerArr.slice(count, 1);
+                console.log("User removed");
+            } 
+            if(count === this.plannerArr.length) {
+                return console.log("User not found");
             }
+
+            count++;
+        }
+    }
+
+    findPerson(name) {
+        let count = 0;
+        while(count < this.plannerArr.length) {
+            if(name === this.plannerArr[count].name) {
+                return `user: ${this.plannerArr[count].name} index: ${count}`
+            } 
+            if(count === this.plannerArr.length) {
+                return console.log("User not found");
+            }
+
+            count++;
+        }
+    }
+
+    printPlanner() {
+        return this.plannerArr;
+    }
+
+    printPerson(position) {
+        let count = 0;
+        while(count < this.plannerArr.length) {
+            if(position === count) {
+                console.log(this.plannerArr[count]);
+            } 
+            if(count === this.plannerArr.length) {
+                return console.log("User not found");
+            }
+
+            count++;
         }
     }
 }
+
+const newPlanner = new Planner();
+newPlanner.savePerson("Jose", 28, 1.75);
+newPlanner.savePerson("Eduardo", 18, 1.80);
+newPlanner.savePerson("Mariana", 20, 1.63);
+newPlanner.savePerson("Matheus", 22, 1.77);
+
+console.log(newPlanner.printPlanner());
+
+
