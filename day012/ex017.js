cb5a3c077f21c93cf727c2f996147c79724d1cbb
ducarv <educarvalho009@gmail.com>
2022-11-16 05:43:17 -0400

    avarage age

---
+++ day012/ex017.js
@@ -0,0 +1,30 @@
+// Intermediário 31- Faça um programa que receba várias idades e que calcule e mostre a média das idades digitadas.
+
+const ageArr = [10, 22, 13, 16, 17, 29, 33, 51, 47, 25];
+
+const avarageAge = ages => {
+    let sumAge = 0;
+    for(let count = 0; count < ages.length; count++) {
+        sumAge += ages[count];
+    }
+
+    const avarage = sumAge / ages.length;
+    return avarage;
+}
+
+console.log(avarageAge(ageArr));
+
+/*
+    <<< EXPLICAÇÃO >>>
+
+    1.  Criamos uma função que recebe como parâmetro o array com os valores que iremos verificar;
+
+    2.  A primeira linha da função temos uma variável que irar acumular os valores de cada posição
+        do vetor;
+    
+    3.  Então iniciamos um laço, que irá acontecer até a variável de controle deixar de ser menor
+        que o comprimento do vetor, dentro desse laço nós acumulamos os valores dos elementos com a
+        variável "sumAge";
+    
+    4. Por fim calculamos a média e retornamos ela;
+*/
