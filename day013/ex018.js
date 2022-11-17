db37a36ec6e8b052d38ec92bd489b07c883336d4
ducarv <educarvalho009@gmail.com>
2022-11-17 05:48:57 -0400

    prime numbers

---
+++ day013/ex018.js
@@ -0,0 +1,30 @@
+// Intermediário 3. Faça uma função que recebe por parâmetro um valor inteiro e positivo
+// e retorna o valor lógico Verdadeiro caso o valor seja primo e Falso em caso contrário.
+
+const findPrimeNumber = num => {
+    if(num < 0) {
+        return "(num) must be higher than zero"
+    }
+
+    if(num % 2 === 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(findPrimeNumber(106));
+
+/*
+    <<< EXPLICAÇÃO >>>
+
+    1.  Criamos uma função que tem como parâmetro um numero inteiro positivo,
+        que será verificado se é primo ou não;
+
+    2.  Primeiramente fazemos uma condição para validar se o número é mesmo e
+        positivo;
+
+    3.  A seguir temos uma condição que verifica se o 'num' tem o resto da visão exata
+        diferente de zero, se for igual a zero o número não é primo e retorna 'false',
+        se o resto for diferente de zero retorna 'true', pois o numero é primo;
+*/
