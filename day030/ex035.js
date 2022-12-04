11dd179db8d742e399d3cbab39146d281634ffb2
ducarv <educarvalho009@gmail.com>
2022-12-04 17:25:28 -0400

    hello world

---
+++ day030/ex035.js
@@ -0,0 +1,12 @@
+function congratulations() {
+    return "Hello World";
+}
+
+console.log(congratulations());
+
+/*
+    THANK YOU SO MUCH
+    Espero que esse respositorio tenha ajudado voce a clarear os conceitos
+    na sua mente e que voce possa passar esse conhecimento para as proximas
+    geracoes.
+*/
